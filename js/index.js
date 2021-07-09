98136ed3c7ef5fa0f3f705bcbea9a35bd8f185d5
Francoviera <vierafranco1@gmail.com>
2021-07-09 14:53:00 -0300

    se solucionaron conflictos con js

--- js/index.js
+++ js/index.js
@@ -34,8 +34,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.querySelector(".buttonCampana").addEventListener("click", function() {
         document.querySelector(".items_notificaciones").classList.toggle("ocultar");
+        document.querySelector(".items_perfil").classList.add("ocultar");
+
     });
     document.querySelector(".navbar-button").addEventListener("click", function() {
         document.querySelector(".items_perfil").classList.toggle("ocultar");
+        document.querySelector(".items_notificaciones").classList.add("ocultar");
+
     });
 });
